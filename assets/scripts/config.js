37b81d6a924f2a62077fa259f7e68003552db3fc
ktresel2 <ktreseler2@gmail.com>
2022-03-11 16:09:32 -0500

    Change back-end address

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -1,17 +1,17 @@
-'use strict'
+"use strict";
 
-let apiUrl
+let apiUrl;
 const apiUrls = {
-  production: 'https://ancient-thicket-87033.herokuapp.com',
-  development: 'http://localhost:4741'
-}
+  production: "https://peloton-tracker.herokuapp.com",
+  development: "http://localhost:4741",
+};
 
-if (window.location.hostname === 'localhost') {
-  apiUrl = apiUrls.development
+if (window.location.hostname === "localhost") {
+  apiUrl = apiUrls.development;
 } else {
-  apiUrl = apiUrls.production
+  apiUrl = apiUrls.production;
 }
 
 module.exports = {
-  apiUrl
-}
+  apiUrl,
+};
